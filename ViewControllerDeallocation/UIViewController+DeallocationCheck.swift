38db56e6073eca8f37f2ce3b4bb023d75b6a8346
Stefan Sut <sutstefan@yahoo.com>
2017-12-13 13:59:40 +0200

    First commit

--- ViewControllerDeallocation/UIViewController+DeallocationCheck.swift
+++ ViewControllerDeallocation/UIViewController+DeallocationCheck.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2017 Stefan Sut. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    public func _checkDeallocation(after delay: TimeInterval = 2.0) {
+        let rootParentViewController = _rootParentViewController
+        
+        if isMovingFromParentViewController || rootParentViewController.isBeingDismissed {
+            let selfType = type(of: self)
+            let disappearance = isMovingFromParentViewController ? "removed from its parent" : "dismissed"
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: { [weak self] in
+                assert(self == nil, "\(selfType) not deallocated after being \(disappearance)")
+            })
+        }
+    }
+    
+    private var _rootParentViewController: UIViewController {
+        var root = self
+        while let parent = root.parent { root = parent }
+        return root
+    }
+    
+}
