5e0f4b175176f2a6d3cdb89ccff33a6c898ffccd
Stefan Sut <sutstefan@yahoo.com>
2017-12-13 13:54:33 +0200

    First commit

---
+++ ViewControllerDeallocation/UIViewController+DeallocationCheck.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+DeallocationCheck.swift
+//  ViewControllerDeallocation
+//
+//  Created by Stefan Sut on 12/13/17.
+//  Copyright © 2017 Stefan Sut. All rights reserved.
+//
+
+import Foundation
